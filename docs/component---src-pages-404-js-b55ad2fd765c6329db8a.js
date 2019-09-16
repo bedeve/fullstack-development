4968e3c55774f3fc14d13a8a085a2e6cacf0bc66
Vasilis Kefallinos <vkefallinos@gmail.com>
2019-09-17 02:15:57 +0300

    add docz build for ghpages

---
+++ docs/component---src-pages-404-js-b55ad2fd765c6329db8a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{965:function(t,e,n){"use strict";n.r(e);var i=n(9);n(1);e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b55ad2fd765c6329db8a.js.map
---
+++ docs/styles-6d1b71275d25771a1487.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{357:function(n,w,o){}}]);
+//# sourceMappingURL=styles-6d1b71275d25771a1487.js.map
