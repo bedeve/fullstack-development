f097ed5575341d427f706b762e18c977a71d62bd
Vasilis Kefallinos <vkefallinos@gmail.com>
2019-09-28 19:04:38 +0300

    fix soundcloud scraper handling

--- packages/app/src/soundcloud.js
+++ packages/app/src/soundcloud.js
@@ -4,30 +4,40 @@ import puppeteer from 'puppeteer'
 
 
 export const searchSoundcloud = async () => {
-    const BASE_URL = 'https://soundcloud.com/'
-    const searchField = ".headerSearch__input"
+  const BASE_URL = 'https://soundcloud.com/'
+  const usernameField = ".soundTitle__usernameText"
+  const searchField = ".headerSearch__input"
+  /** create a browser instance, then a page instance with it */
+  const browser = await puppeteer.launch(
+    {headless: false}
+  )
+  const page = await browser.newPage()
 
-    /** create a browser instance, then a page instance with it */
-    const browser = await puppeteer.launch(
-        {headless: false}
-    )
-    const page = await browser.newPage()
+  await page.goto(`${BASE_URL}`)
 
-    await page.goto(`${BASE_URL}`)
-
-    await page.waitForSelector(searchField)
-    await page.click(searchField)
-    await page.type(searchField, "Bob Marley")
-    await page.keyboard.press('Enter');
-    await page.waitForSelector('.sc-link-dark span')
-    const results =  await page.evaluate(() => {
-      const elements = Array.from(document.querySelectorAll('.sc-link-dark span'))
-      return elements.map(element => {
-        return {
-          name: element.textContent
-        }
-      })
+  await page.waitForSelector(searchField)
+  await page.click(searchField)
+  await page.type(searchField, "Bob Marley")
+  await page.keyboard.press('Enter');
+  const searchListItem = ".sound__body";
+  const itemName = ".sc-link-dark span";
+  const itemUser;
+  const itemImage;
+  const itemUrl;
+  const itemTags;
+  await page.waitForSelector(searchListItem)
+  const results = await page.evaluate(() => {
+    const elements = Array.from(document.querySelectorAll(searchListItem))
+    return elements.map(element => {
+      // console.log(element)
+      return {
+        name: element.querySelector(itemName).textContent,
+        url: element.querySelector(itemUrl).href,
+        img: element.querySelector(itemImage).src
+      }
     })
-    return results
+  })
+  console.log(results)
+  // return results
 }
 
