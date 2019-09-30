9c9d1921012a4224d29299bacde6f27e91fe075c
Eler26 <ellistrongyli@gmail.com>
2019-09-30 19:04:15 +0300

    add puppeteer section

--- packages/app/src/soundcloud.js
+++ packages/app/src/soundcloud.js
@@ -6,6 +6,8 @@ import puppeteer from 'puppeteer'
 export const searchSoundcloud = async () => {
     const BASE_URL = 'https://soundcloud.com/'
     const searchField = ".headerSearch__input"
+    const usernameField = ".soundTitle__usernameText"
+    const url = ".soundTitle__title"
 
     /** create a browser instance, then a page instance with it */
     const browser = await puppeteer.launch(
@@ -19,12 +21,13 @@ export const searchSoundcloud = async () => {
     await page.click(searchField)
     await page.type(searchField, "Bob Marley")
     await page.keyboard.press('Enter');
-    await page.waitForSelector('.sc-link-dark span')
+    await page.waitForSelector('.soundTitle__title')
     const results =  await page.evaluate(() => {
-      const elements = Array.from(document.querySelectorAll('.sc-link-dark span'))
+      const elements = Array.from(document.querySelectorAll('.soundTitle__title'))
       return elements.map(element => {
         return {
-          name: element.textContent
+          name: element.href
+  
         }
       })
     })
