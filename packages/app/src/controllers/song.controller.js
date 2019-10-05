25ef919c643fffcdb0b9dc23405fcc8091cf3c66
Vasilis Kefallinos <vkefallinos@gmail.com>
2019-10-05 15:40:29 +0300

    add controller changes

--- packages/app/src/controllers/song.controller.js
+++ packages/app/src/controllers/song.controller.js
@@ -1,5 +1,5 @@
 import Song from '../models/song.model'
 export const search = (req, res) => {
   const searchQuery = req.query.search
-  res.json({songs: []})
+  res.json({songs: [], search: searchQuery})
 }
