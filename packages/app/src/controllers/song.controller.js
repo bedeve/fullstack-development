a822cf2eceb970c011932ed565fddbf1ca246f9c
Vasilis Kefallinos <vkefallinos@gmail.com>
2019-10-12 00:33:39 +0300

    Add cached search functionality

--- packages/app/src/controllers/song.controller.js
+++ packages/app/src/controllers/song.controller.js
@@ -1,4 +1,5 @@
 import Song from '../models/song.model'
+import Search from '../models/search.model'
 import {searchSoundcloud} from "../scrapers/soundcloud"
 import {searchYoutube} from "../scrapers/youtube"
 
@@ -6,8 +7,40 @@ import {searchYoutube} from "../scrapers/youtube"
 export const search = async (req, res) => {
   const searchQuery = req.query.search_query
   console.log("TCL: search -> searchQuery", searchQuery)
-  const youtubeResults = await searchYoutube(searchQuery)
-  const soundcloudResults = await searchSoundcloud(searchQuery)
-  
-  res.json({soundcloud: soundcloudResults,youtube: youtubeResults , search: searchQuery})
+  const cachedResultsCount = await Search.count({ // check the deprecation warning
+    query: searchQuery
+  })
+  if(cachedResultsCount === 0){
+    const youtubeResults = await searchYoutube(searchQuery)
+      .catch(err=>{
+        console.error("Youtube scraper error")
+        return []
+      })
+    const soundcloudResults = await searchSoundcloud(searchQuery)
+      .catch(err=>{
+        console.error("Soundcloud scraper error")
+        return []
+      })
+    const allResults = youtubeResults.concat(soundcloudResults)
+    const songs = await Song.create(
+      allResults
+    )
+    await Search.create({
+        query: searchQuery, 
+        results: songs?songs.map(song=>song._id):[]  
+    })
+  } 
+  const storedSongs = await Search.find({
+    query: searchQuery
+  }).populate(
+    "results", "name url image"
+  )
+  res.json(storedSongs)
 }
+
+const removeAllSearches = async () => {
+  await Search.deleteMany({})
+  await Song.deleteMany({})
+  console.log("Search and Song collections emptied")
+}
+// removeAllSearches()
