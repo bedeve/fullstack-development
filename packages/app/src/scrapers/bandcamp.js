6db34811f8154a666a45292f0c521b36c726eafb
margaritaorfanidi <orfanidi.margarita@gmail.com>
2019-11-11 14:13:50 +0200

    jjj

--- packages/app/src/scrapers/bandcamp.js
+++ packages/app/src/scrapers/bandcamp.js
@@ -1,16 +1,25 @@
 import puppeteer from 'puppeteer';
 import {scrape} from '../utils/browser/common_scraper'
 const BANDCAMP_BASE_URL = 'https://bandcamp.com/';
+
+
 const bandCampSelectors = {
   searchField: "input.you-autocomplete-me dismiss-tooltip-alt",
   searchListItem: "#pgBd > div.search > div.leftcol > div > ul > li:nth-child(1)",
-  itemName: "#pgBd > div.search > div.leftcol > div > ul > li:nth-child(1) > div > div.heading > a",
-  itemUrl: "#pgBd > div.search > div.leftcol > div > ul > li:nth-child(1) > div > div.heading > a",
-  
+  properties: {
+    name: {
+      selector: "#pgBd > div.search > div.leftcol > div > ul > li:nth-child(2) > div > div.heading > a ",
+      attribute: "text.Content"
+    },
+    url: {
+      selector: "#pgBd > div.search > div.leftcol > div > ul > li:nth-child(1) > div > div.heading > a",
+      attribute: "href"
+    }
+  }
 }
-export const searchBandCamp = async (query) => {
+export const searchMusicBrainz = async (query) => {
   /** create a browser instance, then a page instance with it */
-  
+
   const browser = await puppeteer.launch({
     headless: false
   });
@@ -26,7 +35,8 @@ export const searchBandCamp = async (query) => {
   // to be able to pass variables in the function that will run in the browser
   // we have to add the data after the function and also in the function
   // arguments
-  const results = await page.evaluate(scrape , bandCampSelectors); // pass here any variables you need to access in the evaluate function
+  const results = await page.evaluate(scrape, bandCampSelectors); // pass here any variables you need to access in the evaluate function
   await browser.close()
   return results;
 };
+
